c498c93a450ce7f7baeb26637cde93f7a54e1479
Sarkhamy <ford.roman@gmail.com>
2019-11-18 23:26:49 -0500

    first filling of POS database

--- pos.sql
+++ pos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2019 at 08:09 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Generation Time: Nov 19, 2019 at 04:37 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -18,9 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `pos` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `pos`;
 --
 -- Database: `pos`
 --
@@ -32,11 +29,21 @@ USE `pos`;
 --
 
 CREATE TABLE `category` (
-  `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryID` int(11) NOT NULL,
   `Name` varchar(128) COLLATE utf16_bin NOT NULL,
-  `Tax` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`CategoryID`)
+  `Tax` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`CategoryID`, `Name`, `Tax`) VALUES
+(1, 'Food', 0),
+(2, 'Beer', 6),
+(3, 'Wine', 6),
+(4, 'Candy', 6),
+(5, 'Soft Drinks', 6);
 
 -- --------------------------------------------------------
 
@@ -45,10 +52,23 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `customers` (
-  `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(128) COLLATE utf16_bin NOT NULL,
-  PRIMARY KEY (`CustomerID`)
+  `CustomerID` int(11) NOT NULL,
+  `Name` varchar(128) COLLATE utf16_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `Name`) VALUES
+(1, 'Joe'),
+(2, 'Jill'),
+(3, 'bob'),
+(4, 'Jane'),
+(5, 'barb'),
+(6, 'Sharkiesha'),
+(7, 'Durell'),
+(8, 'Watermellondrea');
 
 -- --------------------------------------------------------
 
@@ -57,17 +77,26 @@ CREATE TABLE `customers` (
 --
 
 CREATE TABLE `products` (
-  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
+  `ProductID` int(11) NOT NULL,
   `Name` varchar(128) COLLATE utf16_bin NOT NULL,
   `Price` double NOT NULL,
   `CategoryID` int(11) NOT NULL,
   `Stock` int(11) NOT NULL,
-  `HeldStock` int(11) NOT NULL,
-  PRIMARY KEY (`ProductID`),
-  UNIQUE KEY `Name` (`Name`),
-  KEY CategoryID (CategoryID),
-  CONSTRAINT `CategoryID` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`)
+  `HeldStock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `Name`, `Price`, `CategoryID`, `Stock`, `HeldStock`) VALUES
+(1, 'American cheese 15 pck', 1.79, 1, 50, 35),
+(2, 'Bread 1 loaf', 0.8, 1, 200, 199),
+(3, 'Ground beef 1/2 Pound', 5, 1, 67, 52),
+(4, 'German stout 1 bottle', 2.99, 2, 200, 199),
+(5, 'American Pale Ale 1 Bottle', 0.99, 2, 500, 299),
+(6, 'red wine 1 bottle', 7.99, 3, 100, 45),
+(7, 'white wine 1 bottle', 7.99, 3, 150, 75);
 
 -- --------------------------------------------------------
 
@@ -76,13 +105,10 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `purchases` (
-  `PurchaseID` int(11) NOT NULL AUTO_INCREMENT,
-  `total` double NOT NULL,
-  `CustomerID` int(11),
-  `DateTime` datetime NOT NULL,
-  PRIMARY KEY (`PurchaseID`),
-  KEY CustomerID (CustomerID),
-  CONSTRAINT `CustomerID` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`)
+  `PurchaseID` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `DateTime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 -- --------------------------------------------------------
@@ -96,14 +122,97 @@ CREATE TABLE `purchase_details` (
   `PurchaseLine` int(11) NOT NULL,
   `ProductID` int(11) NOT NULL,
   `discount` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`PurchaseID`,`PurchaseLine`),
-  KEY ProductID (ProductID),
-  CONSTRAINT `PurchaseID` FOREIGN KEY (`PurchaseID`) REFERENCES `purchases` (`PurchaseID`),
-  CONSTRAINT `ProductID` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`)
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD UNIQUE KEY `Name` (`Name`),
+  ADD KEY `CategoryID` (`CategoryID`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`PurchaseID`),
+  ADD KEY `CustomerID` (`CustomerID`);
+
+--
+-- Indexes for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD PRIMARY KEY (`PurchaseID`,`PurchaseLine`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `PurchaseID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `CategoryID` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`);
+
+--
+-- Constraints for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD CONSTRAINT `CustomerID` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`);
+
+--
+-- Constraints for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD CONSTRAINT `ProductID` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`),
+  ADD CONSTRAINT `PurchaseID` FOREIGN KEY (`PurchaseID`) REFERENCES `purchases` (`PurchaseID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
